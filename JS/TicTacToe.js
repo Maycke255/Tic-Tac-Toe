24e26b5421de0ba1ed506201d7865de4205f12b3
Maycke <reydsonk@gmail.com>
2025-06-06 16:15:46 -0300

    Iniciando JS

--- JS/TicTacToe.js
+++ JS/TicTacToe.js
@@ -1,3 +1,5 @@
+// EFEITOS VISUAIS PARA O CSS
+
 document.querySelectorAll('.add-players').forEach(function (btn) {
     btn.addEventListener('mousemove', function (e) {
         const rect = e.target.getBoundingClientRect();
@@ -57,29 +59,84 @@ document.querySelectorAll(`.reset-score`).forEach(function (btn) {
     });
 });
 
+//VARIAVEIS QUE USAREMOS NO CÓDIGO:
+// Inputs dinamicos dos nomes
+const playerXName = document.getElementById(`name-player-one`)
+const playerOName = document.getElementById(`name-player-two`)
+
+//Inputs dos nomes na tabela de pontuação
+const nameX = document.querySelector(`.player-one`);
+const nameO = document.querySelector(`.player-two`);
+
+//Placar de pontuação
+const scorePlayerX = document.getElementById(`score-player-one`)
+const scorePlayerO = document.getElementById(`score-player-two`)
+
+//Celulas
+const cells = document.querySelectorAll(`cell`)
+
+//Display para exibir os eventos
+const display = document.querySelector(`.display`)
+
+//Condições de vitoria
+const winConditions = [
+    //horizontais
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    //verticais
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    //diagonais
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+//Opções
+let options = ["", "", "", "", "", "", "", "", ""]
+let currentPlayer = "X";
+let gameActive = true;
+
+initializeGame()
+
+function initializeGame () {
+    display.textContent = `Vez do jogador ${currentPlayer}.`
+}
+
 document.getElementById(`add-players-btn`).addEventListener(`click`, function (){
-    const playerX = document.getElementById(`name-player-one`)
-    const playerO = document.getElementById(`name-player-two`)
-    
-    const nameX = document.querySelector(`.player-one`);
-    const nameO = document.querySelector(`.player-two`);
+    if (playerXName.value === `` && playerOName.value === ``) {
+        playerOName.classList.add(`error`)
+        playerXName.classList.add(`error`)
 
-    if (playerX.value === `` && playerO.value === ``) {
-        playerO.classList.add(`error`)
-        playerX.classList.add(`error`)
-
-        playerX.focus()
+        playerXName.focus()
 
         setTimeout(function () { 
-            playerO.classList.remove('error');
-            playerX.classList.remove('error');
+            playerOName.classList.remove('error');
+            playerXName.classList.remove('error');
         }, 1100);
         return;
     }
 
-    nameX.textContent += playerX.value
-    nameO.textContent += playerO.value
+    nameX.textContent += playerXName.value
+    nameO.textContent += playerOName.value
 
-    playerX.value = ``;
-    playerO.value = ``;
+    playerXName.value = ``;
+    playerOName.value = ``;
+})
+
+document.getElementById(`reset-scoreboard-btn`).addEventListener(`click`, function () {
+    scorePlayerX.value = ``
+    scorePlayerO.value = ``
+})
+
+document.getElementById(`choose-new-players`).addEventListener(`click`, function () {
+    nameX.value = ``;
+    nameO.value = ``;
+
+    scorePlayerX.value = ``;
+    scorePlayerO.value = ``;
+
+    playerXName.focus()  
+    playerOName.focus()  
 })
